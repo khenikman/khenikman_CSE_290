805a219b5f944946f371c35cf68401948c0e2155
khenikman <khenikman1@student.ccc.edu>
2026-01-16 12:48:53 -0600

    Laptop Cloned repo works!

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,5 +4,6 @@ using namespace std;
 //change made by Kyle
 int main() {
     cout << "Hello World github FINALLY works :)" << endl;
+    cout << "Sucessfully cloned git repo from main pc to laptop only took like 2 hrs. Super frustrating!" << endl;
     return 0;
 }
