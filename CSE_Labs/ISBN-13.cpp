5732de62ef182d4d4e3785f9e61358bd195c8749
khenikman <khenikman1@student.ccc.edu>
2026-02-25 14:28:01 -0600

    Beautified version of ISBN Solo Project

--- CSE_Labs/ISBN-13.cpp
+++ CSE_Labs/ISBN-13.cpp
@@ -1,10 +1,3 @@
-//Take a string with 12 numbers and dashes
-//string manip to get string with just numbers
-//add each digit multiplied alternating by 1 and 3
-//modulus 10 of sum
-//10 - modulus gives digit 13
-//if this 13 digit code is divisible by list of prime numbers, return category
-//add code to appropriate linked list
 //#include "ISBNTester.h"
 #include <iostream>
 #include <string>
@@ -16,18 +9,18 @@ using namespace std;
 
     struct Node {
         string code;     // ISBN code
-        //string name;     // Book name/title (or whatever "name" means)
-        bool validCode;  // true/false
-        string subject;
-        Node* next;
+        // bool validCode;  // true/false
+        string subject;  // Category for ISBN
+        Node* next;      //Points to next node
 
-        Node(string myData, bool valid ) : code(myData), 
+        Node(string myData) : code(myData), //Constructor for creating nodes
         validCode(valid), subject(""), next(nullptr) {}
 
 
 
     };
 
+    //Creating all the head nodes for the different subjects
     Node* biologyHead = nullptr;
     Node* englishHead = nullptr;
     Node* mathHead = nullptr;
@@ -39,6 +32,7 @@ using namespace std;
     Node* chemistryHead = nullptr;
     Node* uncategorizedHead = nullptr;
 
+    //Appends new isbn nodes to the head nodes of each subject node
     void appendNode(Node*& head, Node* newNode) {
         if (head == nullptr) {
             head = newNode;
@@ -53,9 +47,10 @@ using namespace std;
         cur->next = newNode;
     }
 
+    //Prints the list of nodes
     void printlist(Node* head) {
         
-        Node *referenceHead = head;
+        Node *referenceHead = head; //reference to head node we don't want to mess with that
 
         if (head != nullptr) {
         
@@ -68,18 +63,17 @@ using namespace std;
 
         }
 
-        delete referenceHead;
+        delete referenceHead; //after we add the node we can delete our reference to the head node
     }
 
-//SciElem *sciElem = new SciElem;
+//returns a vector of all the isbn string inputs without the delimeters
 vector<string> parseFile() {
 
     vector<string> all_isbn_inputs;
-    int linecounter = 0;
 
-     ifstream inFile("isbn.txt");  // open file for reading
+     ifstream inFile("isbn.txt");  // open isbn file for reading
 
-    if (!inFile) {
+    if (!inFile) { // makes sure the file opens correctly
         cout << "Error opening file.\n";
         return all_isbn_inputs;
     }
@@ -87,187 +81,152 @@ vector<string> parseFile() {
     string line;
     while (getline(inFile, line)) {
         
-
-        //int thirteenthnum;
         string thirteen_digit_number = line;
 
         for (int i = 0; i < thirteen_digit_number.length() -1; i++) {
             if (thirteen_digit_number.substr(i, 1) == "-") {
-                thirteen_digit_number.erase(i, 1);
+                thirteen_digit_number.erase(i, 1); //parse through each isbn and erase delimeters
                 i--;
             }
         }
-        all_isbn_inputs.push_back(thirteen_digit_number);
-        linecounter++;
+        all_isbn_inputs.push_back(thirteen_digit_number); //add isbn-13 to all_isbn_inputs
     }
-        // parse line here if needed
+        //All lines of file have been parsed through
         inFile.close();
     return all_isbn_inputs;
 
 
 }
 
+//Returns the sum of the first 12 digits and 
+//determines if the isbn is real and its category 
+int is_isbn_element(string thirteen_digit_number) { //takes in a 13 digit code
 
-   
-
-
-int is_isbn_element(string thirteen_digit_number) {
-
-    //cout << isbn_input; 
-
-    //add each digit multiplied alternating by 1 and 3
     int thirteenthnum;
     int sum = 0;
 
     for (int i = 0; i < thirteen_digit_number.length() -1; i++) {
-            sum += stoi(thirteen_digit_number.substr(i, 1));
-            //cout << sum << endl;
+            sum += stoi(thirteen_digit_number.substr(i, 1)); //sums up the first 12 digits
     }
-
-        
-    
-
-    //cout << sum << endl;
 
     if (10 - (sum % 10) != 0) {
-        thirteenthnum = 10 - (sum % 10);
+        thirteenthnum = 10 - (sum % 10); // for an isbn # to be real the sum of all
+                                         // thirteen numbers must be divisible by 10
     }
     else {
-        thirteenthnum = 0;
+        thirteenthnum = 0; // 12 numbers are divisible by 10
     }
-
-    //10 - modulus gives digit 13
-
-    //cout <<thirteen_digit_number.substr(12, 1);
     
-    if (thirteenthnum == stoi(thirteen_digit_number.substr(12, 1))) {
-        return sum;
+    if (thirteenthnum == stoi(thirteen_digit_number.substr(12, 1))) { //checks the last digit
+        return sum; //sum is the prime number we will be checking since this is a real isbn
     }
     else {
-        return 0;
+        return 0; //not a real isbn #
     }
 
     
 }
 
+//Assigns all of the head nodes with their respective isbn elements
 void isbn_element() {
 
-
-
-    vector<string> thirteen_digits = parseFile();
+    vector<string> thirteen_digits = parseFile(); //grabs the list of parsed isbn #'s
     for (int i = 0; i < thirteen_digits.size(); i++) {
+
         string thirteen_digit_number = thirteen_digits[i];
-    if (is_isbn_element(thirteen_digit_number) != 0) {
-        int primeNumber = is_isbn_element(thirteen_digit_number);
+
+        int primeNumber = is_isbn_element(thirteen_digit_number); // assigns the prime number to the check sum of the
+                                                                  // thirteen digit number or 0 if it's not an isbn
     
 
 
 
-    //if this 13 digit code is divisible by list of prime numbers, return category
+     
+    
+    // isbn code we will be adding to the node
     string isbnCode = thirteen_digit_number;
 
-    //cout << isbnCode << " ";
-    
-    //cout << "ISBN: " << isbnCode << " ";
+    // Adding potential ISBN to node
+    Node *newIsbn = new Node(isbnCode);
 
-    long long isbnNumber = stoll(isbnCode);
-
-    Node *newIsbn = new Node(isbnCode, true);
-    //newIsbn->data = isbnCode;
-
-    //if this 13 digit code is divisible by list of prime numbers, return category
-//cout << primeNumber << " ";
+    // if this 13 digit code is divisible by list of prime numbers, assign the respective head node
     switch (primeNumber) {
 
-case 0:
+    case 0:
 
-newIsbn->subject = "Uncategorized";
-appendNode(uncategorizedHead, newIsbn);
-break;
+    newIsbn->subject = "Uncategorized";
+    appendNode(uncategorizedHead, newIsbn);
+    break;
 
-case 61:
+    case 61:
 
-newIsbn->subject = "Biology";
-appendNode(biologyHead, newIsbn);
-break;
+    newIsbn->subject = "Biology";
+    appendNode(biologyHead, newIsbn);
+    break;
 
-case 71:
+    case 71:
 
-newIsbn->subject = "English";
-appendNode(englishHead, newIsbn);
-break;
+    newIsbn->subject = "English";
+    appendNode(englishHead, newIsbn);
+    break;
 
-case 97:
+    case 97:
 
-newIsbn->subject = "Math";
-appendNode(mathHead, newIsbn);
-break;
+    newIsbn->subject = "Math";
+    appendNode(mathHead, newIsbn);
+    break;
 
-case 73:
+    case 73:
 
-newIsbn->subject = "Physics";
-appendNode(physicsHead, newIsbn);
-break;
+    newIsbn->subject = "Physics";
+    appendNode(physicsHead, newIsbn);
+    break;
 
-case 103:
+    case 103:
 
-newIsbn->subject = "Psychology";
-appendNode(psychologyHead, newIsbn);
-break;
+    newIsbn->subject = "Psychology";
+    appendNode(psychologyHead, newIsbn);
+    break;
 
-case 79 :
+    case 79 :
 
-newIsbn->subject = "Computer Science";
-appendNode(csHead, newIsbn);
-break;
+    newIsbn->subject = "Computer Science";
+    appendNode(csHead, newIsbn);
+    break;
 
-case 53 :
+    case 53 :
 
-newIsbn->subject = "Spanish";
-appendNode(spanishHead, newIsbn);
+    newIsbn->subject = "Spanish";
+    appendNode(spanishHead, newIsbn);
 
-break;
+    break;
 
-case 83 :
+    case 83 :
 
-newIsbn->subject = "French";
-appendNode(frenchHead, newIsbn);
+    newIsbn->subject = "French";
+    appendNode(frenchHead, newIsbn);
 
-break;
+    break;
 
-case 113 :
+    case 113 :
 
-newIsbn->subject = "Chemistry";
-appendNode(chemistryHead, newIsbn);
+    newIsbn->subject = "Chemistry";
+    appendNode(chemistryHead, newIsbn);
 
-break;
+    break;
 
-}
-
-
-
+    }
     
 }
 }
-}
 
-//int main() {
-
-// string isbn_input = "9781861972712"; 
-
-
-
-//     isbn_element(isbn_input);
-
-    
-//     return 0;
 
 int main() {
 
   
-    isbn_element();
+    isbn_element(); //stores all isbn nodes in their respective categories
 
-    printlist(biologyHead);
+    printlist(biologyHead); //prints some of the lists
     printlist(mathHead);
     printlist(frenchHead);
 
