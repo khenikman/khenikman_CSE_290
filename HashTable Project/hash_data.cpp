12f6e8a73abea94737d4c84e1f7d4390af4fd393
khenikman <khenikman1@student.ccc.edu>
2026-03-28 13:12:11 -0500

    hash table project complete with comments

--- HashTable Project/hash_data.cpp
+++ HashTable Project/hash_data.cpp
@@ -8,12 +8,13 @@
 
 using namespace std;
 
-int itemCount;
-int bucketSize = 3;
+
+int bucketCount; //stores the amount of buckets in use
+int bucketSize = 3; //amount of the buckets in a vehicle list
 
 
 struct Vehicle {
-    string Model;
+    string Model; //String that will be hashed to store the vehicle objects
     string brand;
     int year;
     int horsepower;
@@ -24,14 +25,13 @@ struct Vehicle {
     string engine;
 };
 
-//Key = Model and Value is the hash of the model
-//unordered_map<string, Vehicle> vehicleTable;
+//Hash Map that originally can store 5 lists holding bucketSize buckets
 vector<list<Vehicle>> hashTable(5);
-//hashTable.resize(5);
 
-int setHashValue(string key);
+//Prototype for getHashValue
+int getHashValue(string key);
 
-void printTable() {
+void printTable() { //prints all vehicles in the hash table
 
     for (int i = 0; i < hashTable.size(); i++) {
     cout << "Bucket " << i << ": ";
@@ -46,38 +46,37 @@ void printTable() {
 }
 
 bool insertInTable(Vehicle vehicle) {
-    int hashvalue = setHashValue(vehicle.Model); //hashes the vehicle model
+    int hashvalue = getHashValue(vehicle.Model); //hashes the vehicle model
     int listcapacity = hashTable.capacity(); // size of vector
-    if (listcapacity*(.75) <= itemCount) { // # of buckets used is >=  75% capacity, double capacity
+    if (listcapacity*(.75) <= bucketCount) { // # of buckets used is >=  75% capacity, then double capacity
 
         hashTable.resize(listcapacity * 2);
 
     }
 
-    int bucket = (hashvalue % (hashTable.capacity() -1)); //bucket #'s we can use are from 0 - capacity
+    int bucket = (hashvalue % (hashTable.capacity() -1)); //bucket #'s we can use are from 0 - (capacity - 1)
     int startPosition = bucket; // first bucket we are checking
     bool elementInserted = false; //element bucket not found
-    //cout << bucket << " ";
-    //while (!elementInserted) {
-    if (hashTable[bucket].size() < bucketSize) {
+
+    if (hashTable[bucket].size() < bucketSize) { //can we fit more in this list
 
         if (hashTable[bucket].empty()) { //current bucket is empty add element
-            itemCount++;
+            bucketCount++; //bucket list is no longer empty
         }
-        hashTable[bucket].push_back(vehicle);
+        hashTable[bucket].push_back(vehicle); //add element
         elementInserted = true;
         cout << "\n" << vehicle.Model << " was added to the table!\n\n";
         return elementInserted;
     }
-    else {
+    else { //current list is full
         while(!elementInserted) { //while element has not been inserted
             if (bucket+1 < listcapacity) { //check if we have more buckets before we have to loop back to the start of the list
                 bucket++; //iterate to the next bucket
-                if (hashTable[bucket].size() < bucketSize) {
+                if (hashTable[bucket].size() < bucketSize) { //checks if next bucket has room
 
                     if (hashTable[bucket].empty() == true) { //check if its empty
 
-                        itemCount++;
+                        bucketCount++; //bucket is no longer epmty
 
                     }
                     hashTable[bucket].push_back(vehicle); //add to that bucket
@@ -87,10 +86,10 @@ bool insertInTable(Vehicle vehicle) {
                 }
             }
             else { //we have gone through the whole list
-                bucket = 0;
+                bucket = 0; //start at beginning
                 if (hashTable[bucket].size() < bucketSize) {
                     if (hashTable[bucket].empty()) {
-                        itemCount++;
+                        bucketCount++;
                     }
                     hashTable[bucket].push_back(vehicle);
                     elementInserted = true;
@@ -98,27 +97,30 @@ bool insertInTable(Vehicle vehicle) {
                     return elementInserted;
                 }
             }
-            if (bucket == startPosition) { //no position to insert
+            if (bucket == startPosition) { //end once we iterate through the entire vector
                 cout << "\n" << "Element could not be inserted" << "\n\n";
                 return false;
             }
         }
 
     }
-    itemCount++;
+    bucketCount++;
 }
 
 
 
-bool deleteInTable(string Model) {
-    int key = setHashValue(Model);
+bool deleteInTable(string Model) { //iterates through the vector to find the element to delete
+    int key = getHashValue(Model); //turns the string for the vehicle model into the key we want
     for (auto bucket = hashTable.begin(); bucket != hashTable.end(); bucket++) {
 
         for(auto car = bucket->begin(); car != bucket->end(); car++) {
 
-            if (setHashValue(car->Model) == key) {
+            if (getHashValue(car->Model) == key) {
                 cout << "\n" << Model << " was deleted from the table!\n\n";
-                bucket->erase(car);
+                bucket->erase(car); //delete the vehicle
+                if (bucket->empty() == true) {
+                    bucketCount--; //bucket empty after deletion
+                }
                 return true;
             }
 
@@ -128,22 +130,22 @@ bool deleteInTable(string Model) {
     return false;
 }
 
-int setHashValue(string key) {
+int getHashValue(string Model) { //return the ASCII hash for the model of a vehicle
     int hashcode = 0;
-    for (char character : key) {
+    for (char character : Model) {
         hashcode += (int) character;
     }
     return hashcode;
 }
 
-bool findInTable(string Model) {
+bool findInTable(string Model) { //iterates through the table to find hash key value pair
 
-    int key = setHashValue(Model);
+    int key = getHashValue(Model);
     for (auto bucket = hashTable.begin(); bucket != hashTable.end(); bucket++) {
 
         for(auto car = bucket->begin(); car != bucket->end(); car++) {
 
-            if (setHashValue(car->Model) == key) {
+            if (getHashValue(car->Model) == key) {
                 cout << "\n" << car->Model << " exists in the table!\n\n";
                 return true;
             }
@@ -156,16 +158,13 @@ bool findInTable(string Model) {
 
 
 
-//returns a vector of all the isbn string inputs without the delimeters
+//returns a vector of vector<<Vehicle>> to be iterated over by the insert function
 vector<Vehicle> parseFile() {
 
-    //vector<string> all_isbn_inputs;
     vector<Vehicle> vehicleinfos;
 
 
-    Vehicle newVehicle = Vehicle();
-
-     ifstream inFile("vehicle_specifications.txt");  // open isbn file for reading
+     ifstream inFile("vehicle_specifications.txt");  // open vehicle file for reading
 
     if (!inFile) { // makes sure the file opens correctly
         cout << "Error opening file.\n";
@@ -175,11 +174,13 @@ vector<Vehicle> parseFile() {
     string line;
     //Model|Brand|Year|Horsepower|MPG_City|MPG_Highway|Drivetrain|Transmission|Engine
     while (getline(inFile, line)) {
-        vector<string> info;
+        vector<string> info; //stores all vehicle info data in an easy way to add to variables
         string vehicleInfo = line;
+
+        Vehicle newVehicle = Vehicle(); //creates a vehicle object to store parsed data
         
-        int startpos = 0;
-        int counter = -1;
+        int startpos = 0; //variable to help with parsing the string
+        int counter = -1; //variable to help with parsing the string
 
         for (int i = 0; i < 9; i++) {
             counter++;
@@ -202,9 +203,6 @@ vector<Vehicle> parseFile() {
 
         vehicleinfos.push_back(newVehicle);
 
-        //vehicleTable
-
-
     }
         //All lines of file have been parsed through
         inFile.close();
@@ -215,13 +213,13 @@ vector<Vehicle> parseFile() {
 }
 int main() {
 
-    vector<Vehicle> vehiclesToAdd = parseFile();
+    vector<Vehicle> vehiclesToAdd = parseFile(); //parses through the vehicle list file
 
     for (int i = 0; i < vehiclesToAdd.size(); i++) {
-        insertInTable(vehiclesToAdd[i]);
+        insertInTable(vehiclesToAdd[i]); //goes through the vector of vehicles and inserts them by hash
     }
 
-    printTable();
+    printTable(); //prints all table values
 
     findInTable("2024 Subaru Forester");
 
